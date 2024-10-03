c652641b9a9efacd3db5765f9f423c1e6cb4231e
Mohamed Aziz Lahmar <92262308+azizlahmar2020@users.noreply.github.com>
2024-10-03 09:13:39 +0100

    last_One

--- resources/views/frontoffice/navbar.blade.php
+++ resources/views/frontoffice/navbar.blade.php
@@ -1,46 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<style>
+    /* Couleur noire pour tous les textes par défaut */
+    .navbar-nav .nav-link {
+        color: #000000 !important;
+    }
 
-    <!-- Font Awesome for Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-        <a href="index.html" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
-            <h2 class="m-0 text-primary">Solartec</h2>
-        </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="/dashboard" class="nav-item nav-link active">Home</a>
-                <a href="#" class="nav-item nav-link">About</a>
-                <a href="/Logement" class="nav-item nav-link">Logement</a>
-                <a href="/Electros" class="nav-item nav-link">Electromenager</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu bg-light m-0">
-                        <a href="#" class="dropdown-item">Logement</a>
-                        <a href="#" class="dropdown-item">Free Quote</a>
-                        <a href="#" class="dropdown-item">Our Team</a>
-                        <a href="#" class="dropdown-item">Testimonial</a>
-                        <a href="#" class="dropdown-item">404 Page</a>
-                    </div>
+    /* Couleur verte pour les éléments actifs et au survol */
+    .navbar-nav .nav-link.active,
+    .navbar-nav .nav-link:hover {
+        color: #4CAF50 !important;
+    }
+
+    /* Texte "Solartec" en vert */
+    .navbar-brand h2 {
+        color: #4CAF50 !important;
+    }
+
+    /* Bouton avec fond vert et texte noir pour "Get A Quote" */
+    .btn-custom-green {
+        background-color: #4CAF50 !important;
+        border-color: #4CAF50 !important;
+    }
+
+    /* Couleur légèrement plus sombre pour le hover sur le bouton */
+    .btn-custom-green:hover {
+        background-color: #45a049 !important;
+        border-color: #45a049 !important;
+    }
+
+    /* Couleur noire pour le texte "Get A Quote" */
+    .btn-custom-green span {
+        color: #000000 !important;
+    }
+
+    /* Couleur blanche pour l'icône dans le bouton */
+    .fa-arrow-right {
+        color: #ffffff !important;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+    <a href="index.html" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
+        <!-- Texte Solartec en vert -->
+        <h2 class="m-0">Solartec</h2>
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <!-- Liens de navigation en noir, deviennent verts lorsqu'ils sont actifs ou survolés -->
+            <a href="/" class="nav-item nav-link active">Home</a>
+            <a href="" class="nav-item nav-link">About</a>
+            <a href="" class="nav-item nav-link">Service</a>
+            <a href="" class="nav-item nav-link">Project</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                <div class="dropdown-menu bg-light m-0">
+                    <a href="/Logement" class="dropdown-item">Logement</a>
+                    <a href="/Electros" class="dropdown-item">Electroménager</a>
+                    <a href="" class="dropdown-item">Our Team</a>
+                    <a href="" class="dropdown-item">Testimonial</a>
+                    <a href="" class="dropdown-item">404 Page</a>
+                    <a href="http://127.0.0.1:8000/fournisseurs" class="dropdown-item">Fournisseurs</a>
+                    <a href="http://127.0.0.1:8000/conseils" class="dropdown-item">Conseils</a>
+
                 </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
             </div>
-            <a href="#" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a>
+            <a href="contact.html" class="nav-item nav-link">Contact</a>
         </div>
-    </nav>
-
-    <!-- Bootstrap JS -->
-</body>
-</html>
+        <!-- Bouton avec fond vert, texte "Get A Quote" en noir, et icône blanche -->
+        <a href="" class="btn btn-custom-green rounded-0 py-4 px-lg-5 d-none d-lg-block">
+            <span>Get A Quote</span>
+            <i class="fa fa-arrow-right ms-3 text-white"></i>
+        </a>
+    </div>
+</nav>
