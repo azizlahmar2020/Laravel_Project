a1328fcc978bf64c4d3f2e5341515e95355fe4c6
Mohamed Aziz Lahmar <92262308+azizlahmar2020@users.noreply.github.com>
2024-10-24 01:00:14 +0100

    done

--- resources/views/frontoffice/CarbonFootprint/show.blade.php
+++ resources/views/frontoffice/CarbonFootprint/show.blade.php
@@ -4,8 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invoice</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+    <title>Invoice</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome for Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <style>
         body {
             font-family: 'Arial', sans-serif;
@@ -120,6 +128,7 @@
 </head>
 
 <body>
+@include('frontoffice.navbar')
 
     <div class="invoice-container">
         <!-- Header Section -->
